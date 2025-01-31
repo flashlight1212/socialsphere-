d49c5c19a213c0ac29000e2599b0c5ab6ab3eb0f
flashlight1212 <flashtee193@gmail.com>
2025-02-01 04:48:52 +0630

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
     databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "socialsphere-eea99",
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
     messagingSenderId: "YOUR_SENDER_ID"
 };
